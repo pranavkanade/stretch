e1319775b976a3d008895a1fe4a5e2b6e402008b
pskanade <k.pranav.apk@gmail.com>
2018-09-07 21:13:49 +0530

    Added code to remove scans (equations) from the problem text using regex

--- scrapper/grab_probems.py
+++ scrapper/grab_probems.py
@@ -2,6 +2,7 @@
 from url_builder import UrlBuilder
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 def grab_problems(slug, url, level):
     html_doc = urllib.request.urlopen(url).read()
@@ -12,11 +13,18 @@ def grab_problems(slug, url, level):
                                      attrs={'class': "difficulty-block"})
 
     problem_text = challenge_body.findAll(['p', 'pre'])
+    problem_text = map(str, problem_text)
+    new_text = []
+    for each_line in problem_text:
+        (each_line, n) = re.subn("(<span .*?</span>)", " TOK ", each_line)
+        new_text.append(BeautifulSoup(each_line, 'html.parser').text)
 
     with open('./data/problems/'+level+'/'+slug+'.txt', 'w') as grabbed_prob:
-        grabbed_prob.writelines(map(str, problem_text))
+        grabbed_prob.write(url)
         grabbed_prob.write("\n\n")
-        grabbed_prob.write(str(difficulty_block))
+        grabbed_prob.writelines(new_text)
+        grabbed_prob.write("\n\n")
+        grabbed_prob.writelines([each_line.text for each_line in difficulty_block])
 
 urlbuilder = UrlBuilder('./data/slugs_with_levels.json')
 levels = ["Easy", "Hard", "Medium"]
